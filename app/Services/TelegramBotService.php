d258ab45b8967e9e874bbf2e6f6f2ac555e1d3a6
Aleksandr Goroshnikov <rogsc2@gmail.com>
2026-02-09 01:09:17 +0300

    added laravel's .gitignore

--- app/Services/TelegramBotService.php
+++ app/Services/TelegramBotService.php
@@ -16,7 +16,7 @@ class TelegramBotService
 
     public function __construct()
     {
-        $this->botToken = config('services.telegram.bot_token');
+        $this->botToken = config('telegram.bot_token');
         $this->apiUrl = "https://api.telegram.org/bot{$this->botToken}";
     }
 
